7331bde91624a4eaabecbe60fa2c36870e6e4956
Davi César <davicesarmorais@gmail.com>
2025-04-04 01:01:21 -0300

    refactor: move database population logic to a separate service

--- app.py
+++ app.py
@@ -3,15 +3,14 @@ from config import Config
 from models import *
 from utils import formatar_curso
 from flask_cors import CORS
-from seed import *
-from apscheduler.schedulers.background import BackgroundScheduler
-from datetime import datetime, timedelta
+
 
 app = Flask(__name__)
 app.config.from_object(Config)
 db.init_app(app)
 
 CORS(app)
+
 
 @app.route("/cursos")
 def get_cursos():
@@ -69,42 +68,8 @@ def find_cursos(nome: str):
     
     return jsonify(cursos_dict)
 
-
-
-class AtualizadorBanco:
-    def __init__(self, intervalo_dias=7):
-        self.intervalo = timedelta(days=intervalo_dias)
-        self.proxima_atualizacao = datetime.now() + self.intervalo
-        self.scheduler = BackgroundScheduler()
-
-    def atualizar_dados(self):
-        print("[DADOS] Iniciando atualização dos dados...")
-        with app.app_context():
-            links = pegar_links_cursos_disponiveis()
-            for curso in links:
-                extrair_e_inserir_dados_url(curso)
-        self.proxima_atualizacao = datetime.now() + self.intervalo
-        print("[DADOS] Dados atualizados com sucesso!")
-
-    def mensagem_diaria(self):
-        dias = (self.proxima_atualizacao - datetime.now()).days
-        if dias > 0:
-            print(f"[DADOS] Próxima atualização em {dias} dias.")
-        else:
-            print("[DADOS] A atualização ocorre hoje!")
-
-    def iniciar_scheduler(self):
-        self.scheduler.add_job(self.atualizar_dados, 'interval', seconds=self.intervalo.total_seconds(), misfire_grace_time=60)
-        self.scheduler.add_job(self.mensagem_diaria, 'cron', hour=12, minute=0, misfire_grace_time=60)
-        self.scheduler.start()
-
-    
 with app.app_context():
     db.create_all()
-
-atualizador = AtualizadorBanco()
-atualizador.atualizar_dados()
-atualizador.iniciar_scheduler()
 
 if __name__ == '__main__':
     app.run(debug=True)
--- utils.py
+++ utils.py
@@ -1,4 +1,8 @@
-from models import Curso
+from bs4 import BeautifulSoup
+import requests
+from bs4.element import NavigableString
+from models import *
+
 
 def formatar_curso(curso: Curso) -> dict:
     periodos = {}
@@ -31,3 +35,113 @@ def formatar_curso(curso: Curso) -> dict:
     }
     
     return curso_dict
+
+
+def extrair_e_inserir_dados_url(url):
+    pagina = requests.get(url)
+    soup = BeautifulSoup(pagina.text, 'html.parser')
+    
+    curso_tag = soup.select_one(
+        'html body.tema-verde div.content div.container div.row div#conteudo.col-sm-12.col-md-10.conteudo h2.titulo.azul-petroleo'
+    )
+    curso_nome = curso_tag.get_text(strip=True)
+    
+    turno_tag = soup.select_one('div.row:nth-child(3) > div:nth-child(1) > ul:nth-child(1) > li:nth-child(4)')
+    turno_nome = turno_tag.get_text(strip=True)
+    
+    formacao_tag = soup.select_one('div.row:nth-child(3) > div:nth-child(1) > ul:nth-child(1) > li:nth-child(2)')
+    formacao_nome = formacao_tag.get_text(strip=True)
+    
+    campus_tag = soup.select_one('html body.tema-verde div.content div.container div.row div#conteudo.col-sm-12.col-md-10.conteudo div.row div.col-xs-12 ul.list-unstyled.list-inline li a')
+    campus_nome = campus_tag.get_text(strip=True)
+    
+    campus_obj = Campus.query.filter_by(nomeCampus=campus_nome).first()
+    if not campus_obj:
+        campus_obj = Campus(nomeCampus=campus_nome)
+        db.session.add(campus_obj)
+        db.session.commit()
+    
+    curso_obj = Curso.query.filter_by(
+        nomeCurso=curso_nome,
+        turno=turno_nome,
+        formacao=formacao_nome,
+        fk_idCampus=campus_obj.idCampus,
+    ).first()
+    
+    if not curso_obj:
+        curso_obj = Curso(
+            nomeCurso=curso_nome,
+            turno=turno_nome,
+            formacao=formacao_nome,
+            fk_idCampus=campus_obj.idCampus
+        )
+        db.session.add(curso_obj)
+        db.session.commit()
+    
+    semestres = soup.select_one('div.corpo:nth-child(3) > div:nth-child(2)')
+    if not semestres:
+        return
+    
+    periodo_num = 1
+    for semestre in semestres:
+        if isinstance(semestre, NavigableString):
+            continue
+        
+        tbody = semestre.find_next('tbody')
+        if not tbody:
+            continue
+        
+        rows = tbody.find_all('tr')
+        for row in rows:
+            cols = row.find_all('td')
+            if len(cols) < 6:
+                continue
+            
+            materia_nome = cols[0].get_text(strip=True)
+            ch = cols[1].get_text(strip=True)
+            try:
+                ch = int(ch)
+            except ValueError:
+                ch = 0
+            
+            materia_obj = Materia.query.filter_by(nomeMateria=materia_nome).first()
+            if not materia_obj:
+                materia_obj = Materia(nomeMateria=materia_nome)
+                db.session.add(materia_obj)
+                db.session.commit()
+            
+            curso_materia_obj = CursoMateria.query.filter_by(
+                fk_idCurso=curso_obj.idCurso,
+                fk_idMateria=materia_obj.idMateria
+            ).first()
+            if not curso_materia_obj:
+                curso_materia_obj = CursoMateria(
+                    ch=ch,
+                    periodo=periodo_num,
+                    fk_idCurso=curso_obj.idCurso,
+                    fk_idMateria=materia_obj.idMateria
+                )
+                db.session.add(curso_materia_obj)
+            else:
+                curso_materia_obj.ch = ch
+                curso_materia_obj.periodo = periodo_num
+        
+        periodo_num += 1
+    db.session.commit()
+
+
+def pegar_links_cursos_disponiveis() -> list[str]:
+    pagina = requests.get("https://estudante.ifpb.edu.br/cursos/")
+    soup = BeautifulSoup(pagina.text, "html.parser")
+    content = soup.select_one('#conteudo')
+    elementos = content.find_all(recursive=False)
+
+    links = set()
+
+    for elemento in elementos[3:]:
+        for link in elemento.find_all('a'):
+            href = link.get("href")
+            if href:
+                links.add(f"https://estudante.ifpb.edu.br{href}")
+
+    return list(links)
